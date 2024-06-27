b3bc8c945bd741d204096b5852f8253c2b018095
TarekAlBalhawan <tarek1fullstackdeveloper@gmail.com>
2024-06-27 12:38:17 +0200

    first commit

---
+++ ownjs.js
@@ -0,0 +1,18 @@
+ // Initialization for ES Users
+import { Dropdown, initMDB } from "mdb-ui-kit";
+
+initMDB({ Dropdown });
+
+// Initialization for ES Users
+import { Dropdown, Ripple, initMDB } from "mdb-ui-kit";
+
+initMDB({ Dropdown, Ripple });
+
+// Initialization for ES Users
+import { Modal, Ripple, initMDB } from "mdb-ui-kit";
+
+initMDB({ Modal, Ripple });
+
+AOS.init();
+
+
